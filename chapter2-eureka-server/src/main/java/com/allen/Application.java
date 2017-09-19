804a7c29bc52db78ff4a50e32332db2f51199e31
allen <buguoduan@gmail.com>
2017-09-19 23:13:18 +0800

    spring cloud eureka 学习

---
+++ chapter2-eureka-server/src/main/java/com/allen/Application.java
@@ -0,0 +1,19 @@
+package com.allen;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ *  在默认设置下，该服务注册中心也会将自己作为客户端来尝试注册它自己
+ */
+// 启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
